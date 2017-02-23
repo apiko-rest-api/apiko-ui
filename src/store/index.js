a4ffe078ff320047f50fb8ca3652ba493ebf9c67
Simon <kartsims@gmail.com>
2017-02-23 18:31:48 +0100

    remove layer on top of axios

--- src/store/index.js
+++ src/store/index.js
@@ -5,6 +5,11 @@ Vue.use(Vuex)
 import axios from 'axios'
 import localStorage from 'store'
 
+// axios instance with API's config
+const api = axios.create({
+  timeout: 2000
+})
+
 export default new Vuex.Store({
   state: {
     loading: false,
@@ -34,11 +39,7 @@ export default new Vuex.Store({
       state.loading = truth
     },
     'SHOW_DOCS' (state, payload) {
-      if (payload) {
-        state.showDocs = payload
-      } else {
-        state.showDocs = !state.showDocs
-      }
+      state.showDocs = payload || !state.showDocs
       // store to local storage
       localStorage.set('showDocs', state.showDocs)
     },
@@ -62,7 +63,10 @@ export default new Vuex.Store({
         state.apiUrl += '/' + payload.prefix
       }
       // set axios base URL
-      axios.defaults.baseURL = state.apiUrl
+      api.defaults.baseURL = state.apiUrl
+      api.defaults.params = {
+        secret: payload.secret
+      }
       // store to local storage
       localStorage.set('config', payload)
     }
@@ -70,86 +74,16 @@ export default new Vuex.Store({
   actions: {
     // connect to the API
     connect ({ commit, dispatch, state }, opts) {
-      console.log('Connecting to: %s, %s a secret...', state.apiUrl, (state.config.secret ? 'with' : 'without'))
-
-      return dispatch('get', {
-        path: '/apiko/setup'
-      }).then(res => {
+      return api.get('/apiko/setup').then(res => {
         commit('SETUP', res.data)
         commit('ORIGINAL_SETUP', res.data)
 
-        return dispatch('get', {
-          path: '/apiko/core'
-        }).then(res => {
+        return api.get('/apiko/core').then(res => {
           commit('CORE', res.data)
         }).catch(err => {
           console.log('Tried to get core, but failed.', err)
         })
       })
-    },
-    // AJAX request, don't use directly, use the wrappers below
-    request ({ commit, state }, opts) {
-      var config = {}
-
-      if (!opts.args) {
-        opts.args = {}
-      }
-
-      console.log(opts.method.toUpperCase(), opts.path, 'Params:', opts.args)
-
-      if (opts.loading !== false) {
-        commit('toggleLoading', true)
-      }
-
-      // prepare path
-      let url = state.apiUrl + opts.path
-      if (state.config.secret) {
-        url += '?secret=' + state.config.secret
-      }
-
-      return axios[opts.method](url, opts.args, config)
-        .then((response) => {
-          console.log('HTTP OK:', response)
-
-          if (opts.loading !== false) {
-            commit('toggleLoading', false)
-          }
-
-          return response
-        })
-        .catch((error) => {
-          console.warn('HTTP ERR:', error)
-
-          if (opts.loading !== false) {
-            commit('toggleLoading', false)
-          }
-
-          return error
-        })
-    },
-
-    // GET
-    get ({ dispatch }, opts) {
-      opts.method = 'get'
-      return dispatch('request', opts)
-    },
-
-    // POST
-    post ({ dispatch }, opts) {
-      opts.method = 'post'
-      return dispatch('request', opts)
-    },
-
-    // PUT
-    put ({ dispatch }, opts) {
-      opts.method = 'put'
-      return dispatch('request', opts)
-    },
-
-    // DELETE
-    delete ({ dispatch }, opts) {
-      opts.method = 'delete'
-      return dispatch('request', opts)
     }
   }
 })
