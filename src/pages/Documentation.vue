49fdec5297443307a13254a6160619f8ef7dd57e
Miroslav Kaspar <k@e0.cz>
2017-04-27 12:09:09 +0100

    Prettified the Stack Overflow part a bit.

--- src/pages/Documentation.vue
+++ src/pages/Documentation.vue
@@ -1,6 +1,5 @@
 <template>
-  <div>
-    <br><!-- FIXME with something nicer than br --><br>
+  <div class="page padded">
     <div class="columns">
       <div class="column is-10 is-offset-1">
         <div class="control is-grouped">
@@ -19,50 +18,44 @@
       </div>
     </div>
 
-    <div class="columns">
-      <div class="column">
-        <div class="hero-body">
-          <h2 class="title has-text-centered">
-            Guide Topics
-          </h2>
-          <hr>
-          <div class="content">
-            <ul>
-              <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
-              <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
-              <li>
+    <div class="card">
+      <div class="card-content">
+        <div class="columns">
+          <div class="column">
+              <h2 class="title has-text-centered">Guide Topic
+              </h2>
+              <hr>
+              <div class="content">
                 <ul>
+                  <li>In fermentum leo eu lectus mollis, quis dictum mi aliquet.</li>
+                  <li>Morbi eu nulla lobortis, lobortis est in, fringilla felis.</li>
                   <li>
-                    In fermentum leo eu lectus mollis, quis dictum mi aliquet.
+                    <ul>
+                      <li>
+                        In fermentum leo eu lectus mollis, quis dictum mi aliquet.
+                      </li>
+                    </ul>
                   </li>
+                  <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
+                  <li>Ut non enim metus.</li>
                 </ul>
-              </li>
-              <li>Aliquam nec felis in sapien venenatis viverra fermentum nec lectus.</li>
-              <li>Ut non enim metus.</li>
-            </ul>
+            </div>
           </div>
-        </div>
-      </div>
-      <div class="column">
-        <div class="hero-body">
-          <h2 class="title has-text-centered">
-            Stack Overflow Q/A
-          </h2>
-          <hr>
-        </div>
-        <div class="content">
-          <div v-for="q in questions">
-            <div v-on:click="goToLink(q.link)" class="clickable">
-              <!-- may need fixing to prevent injection, but helps to display html entities -->
-              <h1 v-html="q.title"></h1>
-              <p>
-                Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla accumsan, metus ultrices
-                eleifend
-                gravida, nulla nunc varius lectus, nec rutrum justo nibh eu lectus. Ut vulputate semper
-                dui.
-                Fusce erat odio, sollicitudin vel erat vel, interdum mattis neque.
-              </p>
-              <br>
+          <div class="column">
+              <h2 class="title has-text-centered">Stack Overflow Q/A</h2>
+              <hr>
+            <div class="content">
+              <div v-if="questions.length">
+                <div class="question" v-for="q in questions">
+                  <h2><a v-html="q.title" :href="q.link" target="_blank"></a></h2>
+                  <p class="tags">
+                    <span v-if="q.is_answered" class="tag is-primary">Answered</span>
+                    <span v-for="tag in q.tags" class="tag is-light">{{tag}}</span>
+                  </p>
+                  <p>{{q.answer_count}} answers, asked {{q.creation_date | datetime}} by {{q.owner.display_name}}</p>
+                </div>
+              </div>
+              <div v-else class="has-text-centered">No questions matching this criteria.</div>
             </div>
           </div>
         </div>
@@ -73,6 +66,8 @@
 
 <script>
   import axios from 'axios'
+  import moment from 'moment'
+
   export default {
     data () {
       return {
@@ -84,25 +79,37 @@
       this.so()
     },
     methods: {
-      goToLink (url) {
-        // http://stackoverflow.com/a/11384018/1351857
-        var win = window.open(url, '_blank')
-        win.focus()
-      },
       so () {
         var tags = this.query
         axios.get('https://api.stackexchange.com/2.2/questions?site=stackoverflow&order=desc&sort=activity&filter=default&tagged=' + tags)
-            .then((res) => {
-              console.log(res.data)
-              this.questions = res.data.items
-            })
+        .then((res) => {
+          this.questions = res.data.items
+        })
+      }
+    },
+    filters: {
+      datetime (timestamp) {
+        return moment.unix(timestamp).format('MMM Do YYYY')
       }
     }
   }
 </script>
 
 <style scoped>
-  .clickable {
-    cursor: pointer
-  }
+.question {
+  margin-bottom: 32px;
+}
+
+.question .tag {
+  margin-right: 6px;
+}
+
+.question h2 {
+  margin-top: 0;
+  margin-bottom: 6px;
+}
+
+.question p.tags {
+  margin-bottom: 6px;
+}
 </style>
